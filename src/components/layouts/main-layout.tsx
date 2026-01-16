9887b601fe075a6688f799ef8678aa97587b913e
Codey Huynh <baohq11@gmail.com>
2026-01-16 23:07:58 +0700

    feat: enhance header styling for improved visibility and user experience

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -51,7 +51,7 @@ export default function MainLayout({
     <SidebarProvider defaultOpen={true}>
       <AppSidebar />
       <SidebarInset>
-        <header className='flex h-16 shrink-0 items-center gap-2'>
+        <header className='sticky top-0 z-30 flex h-16 shrink-0 items-center gap-2 border-b bg-background/50 backdrop-blur'>
           <div className='flex items-center gap-2 px-4'>
             <SidebarTrigger className='-ml-1' />
             <Separator
